6967d7e492aa4734c21d03533fd6326f7d633ced
Lucas Portella <lucas.portella.santos@santander.com.br>
2022-04-21 20:19:43 -0300

    Initial Commit

---
+++ Shared/async_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  async_imageApp.swift
+//  Shared
+//
+//  Created by Lucas Portella Santos on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct async_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
