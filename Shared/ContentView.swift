20e91bccbfd48390cb25c407eedf96b51d967c3c
Lucas Portella <lucas.portella.santos@santander.com.br>
2022-04-21 23:59:32 -0300

    Added AsyncImage to download images. Add simple animations to the downloaded image.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        AsyncImage(url: URL(string: imageURL), scale: 3.0, transaction: .init(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            if let image = phase.image {
+                image
+                    .fitImage()
+                    .transition(.scale)
+            } else if phase.error != nil {
+                Image(systemName: "ant.circle.fill")
+                    .setAsIcon()
+                    .foregroundColor(.red)
+                    .transition(.scale)
+            } else {
+                Image(systemName: "photo.circle.fill")
+                    .setAsIcon()
+                    .foregroundColor(.cyan)
+            }
+        }
+    }
+}
+
+extension Image {
+    func fitImage() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func setAsIcon() -> some View {
+        self
+            .fitImage()
+            .frame(maxWidth: 128)
+            .opacity(0.7)
     }
 }
 
